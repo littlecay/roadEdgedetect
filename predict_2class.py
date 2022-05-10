4fc931fe35f91a6df55e379b08fff61424f3a557
littlecay <littlesummercat@gmail.com>
2022-05-10 15:07:33 +0800

    add test video

--- predict_2class.py
+++ predict_2class.py
@@ -6,7 +6,6 @@ import os
 import random
 import argparse
 import numpy as np
-import cv2
 
 from torch.utils import data
 from datasets import VOCSegmentation, Cityscapes, cityscapes
@@ -21,14 +20,20 @@ import matplotlib
 import matplotlib.pyplot as plt
 from glob import glob
 
+import cv2
+import imageio
+import skimage.io
+import pylab
+
 def get_argparser():
     parser = argparse.ArgumentParser()
 
     # Datset Options
-    parser.add_argument("--input", type=str, default='./',
+    parser.add_argument("--input", type=str, default='./test',
+                        # required=True,
                         help="path to a single image or image directory")
-    parser.add_argument("--dataset", type=str, default='uas',
-                        choices=['voc', 'cityscapes', 'uas'], help='Name of training set')
+    parser.add_argument("--dataset", type=str, default='cityscapes',
+                        choices=['voc', 'cityscapes'], help='Name of training set')
 
     # Deeplab Options
     available_models = sorted(name for name in network.modeling.__dict__ if name.islower() and \
@@ -43,7 +48,7 @@ def get_argparser():
     parser.add_argument("--output_stride", type=int, default=16, choices=[8, 16])
 
     # Train Options
-    parser.add_argument("--save_val_results_to", default=None,
+    parser.add_argument("--save_val_results_to", default='./test',
                         help="save segmentation results to the specified dir")
 
     parser.add_argument("--crop_val", action='store_true', default=False,
@@ -53,9 +58,9 @@ def get_argparser():
     parser.add_argument("--crop_size", type=int, default=513)
 
     
-    parser.add_argument("--ckpt", default='./checkpoints/best/best_deeplabv3plus_resnet50_UAS_os16.pth', type=str,
+    parser.add_argument("--ckpt", default='./checkpoints/best_deeplabv3plus_resnet50_cityscapes_os16.pth', type=str,
                         help="resume from checkpoint")
-    parser.add_argument("--gpu_id", type=str, default='0',
+    parser.add_argument("--gpu_id", type=str, default='0,1,2,3',
                         help="GPU ID")
     return parser
 
@@ -65,9 +70,6 @@ def main():
         opts.num_classes = 21
         decode_fn = VOCSegmentation.decode_target
     elif opts.dataset.lower() == 'cityscapes':
-        opts.num_classes = 19
-        decode_fn = Cityscapes.decode_target
-    elif opts.dataset.lower() == 'uas':
         opts.num_classes = 2
         decode_fn = Cityscapes.decode_target
 
@@ -123,45 +125,49 @@ def main():
     if opts.save_val_results_to is not None:
         os.makedirs(opts.save_val_results_to, exist_ok=True)
 
-    src_video = cv2.VideoCapture('37b49c1476b7cf8b9f178ff65da433fc.mp4')
-    fps = src_video.get(cv2.CAP_PROP_FPS)
-    size = (int(src_video.get(cv2.CAP_PROP_FRAME_WIDTH)),
-            int(src_video.get(cv2.CAP_PROP_FRAME_HEIGHT)))
-
-    videoWriter = cv2.VideoWriter(
-        "output.mp4",
-        cv2.VideoWriter_fourcc(*"mp4v"),  # 编码器
-        fps,
-        size
-    )
+    fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+    cap = cv2.VideoCapture('37b49c1476b7cf8b9f178ff65da433fc.mp4')
+    fps = int(round(cap.get(cv2.CAP_PROP_FPS)))
+    width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+    height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+    out = cv2.VideoWriter('save_37.mp4', fourcc, fps, (width, height))
+    pred_img = np.zeros((height, width, 3)).astype(np.uint8)
 
     with torch.no_grad():
         model = model.eval()
-        while 1:
-            _, frame = src_video.read()
-            if frame is None:
+
+        while(cap.isOpened()):
+            ret, frame = cap.read()
+            # cv2.imshow('input', frame)
+            if (ret):
+                img_ori = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+
+                # for img_path in tqdm(image_files):
+                #     ext = os.path.basename(img_path).split('.')[-1]
+                #     img_name = os.path.basename(img_path)[:-len(ext)-1]
+                #     img = Image.open(img_path).convert('RGB')
+                img = transform(img_ori).unsqueeze(0) # To tensor of NCHW
+                # img = torch.from_numpy(img_ori).unsqueeze(0)
+                img = img.to(device)
+
+                pred_mask = model(img).max(1)[1].cpu().numpy()[0] # HW
+                # colorized_preds = decode_fn(pred).astype('uint8')
+
+                for i in range(3):
+                    pred_img[:,:,i] = (img_ori[:,:,i] * pred_mask)
+
+                out.write(pred_img)
+
+            else:
                 break
-            img = transform(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)).unsqueeze(0)
-            img2 = img.copy()# To tensor of NCHW
-            img = img.to(device)
 
-            pred = model(img).max(1)[1].cpu().numpy()[0] # HW
-            gray = cv2.cvtColor(pred, cv2.COLOR_BGR2GRAY)
-            ret, binary = cv2.threshold(gray, 30, 255, 0)
-            contour, h = cv2.findContours(binary, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+        cap.release()
+        out.release()
+        cv2.destroyAllWindows()
 
-            for i in range(len(contour)):
-                if cv2.contourArea(contour[i]) > 2000:
-                    approx = cv2.approxPolyDP(contour[i], 0.005, True)
-                    img2 = cv2.drawContours(img2, [approx], 0, (255, 255, 255), 5)
-            # colorized_preds = decode_fn(pred).astype('uint8')
-                    videoWriter.write(img2)
-
-        src_video.release()
-        videoWriter.release()
-
-
-
+        # colorized_preds = Image.fromarray(colorized_preds)
+        # if opts.save_val_results_to:
+        #     colorized_preds.save(os.path.join(opts.save_val_results_to, img_name+'_label.png'))
 
 if __name__ == '__main__':
     main()
